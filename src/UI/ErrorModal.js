1201038f5a096916054cd6fd806ba5568ffd0795
Pravesh <pritamyadav70923@gmail.com>
2023-07-12 23:01:13 +0530

    react portal

--- src/UI/ErrorModal.js
+++ src/UI/ErrorModal.js
@@ -1,19 +1,40 @@
-// import classes from "../components/InputForm.module.css";
-import classes from "./ErrorModal.module.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./ErrorModal.module.css";
 import Card from "./Card";
 const ErrorModal = (props) => {
+  const BackDrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onClick}></div>;
+  };
+
+  const ModalOverlay = (props) => {
+    return (
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <button className={classes.button} onClick={props.onClick}>
+            Okay
+          </button>
+        </footer>
+      </Card>
+    );
+  };
   return (
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={classes.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={classes.actions}>
-        <button className={classes.button} onClick={props.onClick}>Okay</button>
-      </footer>
-    </Card>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onClick={props.onClick}></BackDrop>,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay title={props.title} message={props.message} onClick={props.onClick}></ModalOverlay>,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 export default ErrorModal;
